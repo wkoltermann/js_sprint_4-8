2142def9f8efbcf647370598f7d03438cba11add
wkoltermann <302128186@student.rocmondriaan.nl>
2021-03-30 13:58:13 +0200

    an update or smth

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("Good, It didn't crash");
+console.log(`Good, It didn't crash`);
 
 const theBody = document.body;
 
@@ -10,9 +10,9 @@ const page1Changer = document.querySelector(`.switch_page_1`);
 
 const page2Changer = document.querySelector(`.switch_page_2`);
 
-const ulrPage1 = "index.html";
+const ulrPage1 = `index.html`;
 
-const ulrPage2 = "page_2.html";
+const ulrPage2 = `page_2.html`;
 
 function ChangeToPageTwo() {
   window.location = ulrPage2
@@ -24,7 +24,7 @@ function ChangeToPageOne() {
 
 console.log(page1Changer);
 if (page1Changer) { // Changing from page 1 to page 2 by using a function
-    page1Changer.addEventListener(`click`,  ChangeToPageTwo);
+    page1Changer.addEventListener(`click`, ChangeToPageTwo);
 }
 
 console.log(page2Changer);
